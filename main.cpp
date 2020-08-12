8bc197c6bb34be1ffef6947f8ea1c511b0455270
Marcos <marcosleonr03@gmail.com>
2020-08-11 21:37:33 -0500

    main

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+	int entero =45;
+	bool boolean=false;
+	char caracter=64;
+	double doble= 85.6;
+
+	cout<<entero<<endl;
+	cout<<boolean<<endl;
+	cout<<caracter<<endl;
+	cout<<doble<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
